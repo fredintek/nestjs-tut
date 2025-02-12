e353eae5a633c43fc827162cb190ef445af916b0
Arinze Dibor Alfred <arinze.diboralfred@neareasttech.local>
2025-02-12 09:50:17 +0200

    finished up env tut

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,28 +7,39 @@ import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TagsModule } from './tags/tags.module';
 import { MetaOptionsModule } from './meta-options/meta-options.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import appConfig from './config/app.config';
+import databaseConfig from './config/database.config';
+import environmentValidation from './config/environment.validation';
 
+const ENV = process.env.NODE_ENV;
 @Module({
   imports: [
     UsersModule,
     PostsModule,
     AuthModule,
-    TypeOrmModule.forRootAsync({
-      imports: [],
-      inject: [],
-      useFactory: () => ({
-        type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'postgres',
-        password: 'alfred',
-        autoLoadEntities: true,
-        database: 'nestjs-blog',
-        synchronize: true,
-      }),
-    }),
     TagsModule,
     MetaOptionsModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        synchronize: configService.get('database.synchronize'),
+        autoLoadEntities: configService.get('database.autoloadEntities'),
+        host: configService.get('database.host'),
+        port: +configService.get('database.port'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        database: configService.get('database.database'),
+      }),
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: !ENV ? '.env.production' : `.env.${ENV}`,
+      load: [appConfig, databaseConfig],
+      validationSchema: environmentValidation,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
